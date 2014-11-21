8578bcbe5b6f5e11bc20261aeb86c39de2069dc2
Sergey Vinogradov <pettsonchello@gmail.com>
2014-11-21 21:21:54 +0300

    Реализован функционал

--- js/main.js
+++ js/main.js
@@ -1,15 +1,15 @@
 $(document).ready(function() {
 
-  $('.b-last-videos__slider').bxSlider({
+  /*$('.b-last-videos__slider').bxSlider({
     slideWidth: 300,
     slideMargin: 20,
     maxSlides: 3,
     pager: false,
     nextText: '',
     prevText: ''
-  });
+  });*/
 
-  $('.b-preview').each(function() {
+  /*$('.b-preview').each(function() {
     var $this = $(this);
 
     $this.append('<img src="' + $this.data('img-preview') + '" class="b-preview__image">');
@@ -18,15 +18,15 @@ $(document).ready(function() {
         '<a href="' + $this.data('img') + '" class="b-preview__actions__zoom"></a>' + 
         '<a href="' + $this.data('src') + '" class="b-preview__actions__src"></a>' + 
       '</div>');
-  });
+  });*/
 
-  $('.b-welcome-gallery__slider').bxSlider({
+  /*$('.b-welcome-gallery__slider').bxSlider({
     slideWidth: 140,
     slideMargin: 20,
     maxSlides: 3,
     pager: false,
     nextText: '',
     prevText: ''
-  });
+  });*/
 
 });
